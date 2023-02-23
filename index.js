308d16abcc0e9ac54f923a29f6719ad7ebbe9ff1
the.sholaakanbi <oluwalonshola6@gmail.com>
2023-02-23 03:40:52 -0800

    new upadtes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 //error route
 // app.use(notFound)
 app.use((req,res) => {
-    res.status(404).send('Route does not exist')
+    res.status(404).send(`Route not found try <a href='/api/v1/teams'>FOOTBALL API</a>`)
 });
 
 
